537cd004d55e222ecce320ee34f7b4ba4a914ec2
Samuel Narh <samuelnarh38@gmail.com>
2024-03-05 12:28:17 +0000

    connected to the  api and returned a promise

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+//my end point(Backend endpoint)
+const BASE_URL = "http://127.0.0.1:8000/";
+
+const App = () => {
+  const [posts, SetPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + "blog/all")
+      .then((response) => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }
+        throw response;
+      })
+      .then((data) => {
+        return data.reverse();
+      })
+      .then((data) => {
+        SetPosts(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
